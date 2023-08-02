69dc0e5a2cf2a823081b9dfa4bb66e2c95d287b1
tanmoy-repo <tanmoydas.ece24@gmail.com>
2023-08-02 14:18:59 +0530

    initial commit

---
+++ src/main/java/com/gitgenius/GreetRestController.java
@@ -0,0 +1,23 @@
+package com.gitgenius;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class GreetRestController {
+	
+	@Autowired
+	private Environment env;
+	
+	@GetMapping("/greet")
+	public String getGreeting() {
+		
+		String port = env.getProperty("server.port");
+		
+		String msg ="Good evening!! (from server port "+port +")";
+		return msg;
+	}
+
+}
